68d643719c2c55ea1d70e5ace6d169a318f6d7c2
Bruno Almeida <bruno.correia.almeida@gmail.com>
2016-11-16 12:27:32 +0000

    Add script to test model and figures

---
+++ src/landmarks/test_net.py
@@ -0,0 +1,165 @@
+import os
+
+import numpy as np
+from pandas.io.parsers import read_csv
+from sklearn.utils import shuffle
+
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+
+DATA_PATH = '../../data'
+FTRAIN = '../../data/landmarks/training.csv'
+FTEST = '../../data/landmarks/test.csv'
+
+
+########################
+########################
+########################
+
+
+def load(test=False, cols=None):
+    """Loads data from FTEST if *test* is True, otherwise from FTRAIN.
+    Pass a list of *cols* if you're only interested in a subset of the
+    target columns.
+    """
+    fname = FTEST if test else FTRAIN
+    df = read_csv(os.path.expanduser(fname))  # load pandas dataframe
+
+    # The Image column has pixel values separated by space; convert
+    # the values to numpy arrays:
+    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+
+    if cols:  # get a subset of columns
+        df = df[list(cols) + ['Image']]
+
+    print(df.count())  # prints the number of values for each column
+    df = df.dropna()  # drop all rows that have missing values in them
+
+    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    X = X.astype(np.float32)
+
+    if not test:  # only FTRAIN has any target columns
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
+        X, y = shuffle(X, y, random_state=42)  # shuffle train data
+        y = y.astype(np.float32)
+    else:
+        y = None
+
+    return X, y
+
+def load2d(test=False, cols=None):
+    X, y = load(test=test)
+    X = X.reshape(-1, 1, 96, 96)
+    return X, y
+
+##########################3
+##########################3
+##########################3
+
+
+from nolearn.lasagne import BatchIterator
+
+class FlipBatchIterator(BatchIterator):
+    flip_indices = [
+        (0, 2), (1, 3),
+        (4, 8), (5, 9), (6, 10), (7, 11),
+        (12, 16), (13, 17), (14, 18), (15, 19),
+        (22, 24), (23, 25),
+        ]
+
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, :, ::-1]
+
+        if yb is not None:
+            # Horizontal flip of all x coordinates:
+            yb[indices, ::2] = yb[indices, ::2] * -1
+
+            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+            for a, b in self.flip_indices:
+                yb[indices, a], yb[indices, b] = (
+                    yb[indices, b], yb[indices, a])
+
+        return Xb, yb
+
+
+##########################
+##########################
+##########################
+
+
+import theano
+
+def float32(k):
+    return np.cast['float32'](k)
+
+
+class AdjustVariable(object):
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
+
+
+#####################
+#####################
+#####################
+
+
+#X, y = load2d()  # load 2-d data
+#net5.fit(X, y)
+
+import cPickle as pickle
+
+net5 = pickle.load(open("models/net5.pickle", 'rb'))
+
+
+
+import matplotlib.pyplot as pyplot
+
+train_loss = np.array([i["train_loss"] for i in net5.train_history_])
+valid_loss = np.array([i["valid_loss"] for i in net5.train_history_])
+pyplot.plot(train_loss[:750], linewidth=3, label="train")
+pyplot.plot(valid_loss[:750], linewidth=3, label="valid")
+pyplot.grid()
+pyplot.legend()
+pyplot.xlabel("epoch")
+pyplot.ylabel("loss")
+pyplot.ylim(1e-3, 1e-2)
+pyplot.yscale("log")
+pyplot.show()
+#pyplot.savefig("training.png")
+
+
+
+def plot_sample(x, y, axis):
+    img = x.reshape(96, 96)
+    axis.imshow(img, cmap='gray')
+    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
+
+X, _ = load2d(test=True)
+y_pred = net5.predict(X)
+
+fig = pyplot.figure(figsize=(6, 6))
+fig.subplots_adjust(
+    left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+for i in range(16):
+    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+    plot_sample(X[60+i], y_pred[60+i], ax)
+
+pyplot.show()
